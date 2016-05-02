fd23174c3199d8ef83ebad2f4c94c7c868466c08
Eric Ma <ericmajinglong@gmail.com>
2016-05-02 14:20:47 -0400

    added username and email to git configuration

--- install.sh
+++ install.sh
@@ -3,3 +3,6 @@ ln -svf "$HOME/dotfiles/.bash_profile" ~
 ln -svf "$HOME/dotfiles/.bashrc" ~
 ln -svf "$HOME/dotfiles/.condarc" ~
 ln -svf "$HOME/dotfiles/Preferences.sublime-settings" "$HOME/Library/Application Support/Sublime Text 3/Packages/User"
+
+git config --global user.name Eric J. Ma
+git config --global user.email ericmajinglong@gmail.com
