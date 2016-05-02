c7b5ca74104e0ce20ef41165bbd9bb128d098ddd
Eric Ma <ericmajinglong@gmail.com>
2016-05-02 14:19:19 -0400

    added back sublime text settings; keep it under version control

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@
 ln -svf "$HOME/dotfiles/.bash_profile" ~
 ln -svf "$HOME/dotfiles/.bashrc" ~
 ln -svf "$HOME/dotfiles/.condarc" ~
-# ln -svf "$HOME/dotfiles/Preferences.sublime-settings" "$HOME/Library/Application Support/Sublime Text 3/Packages/User"
+ln -svf "$HOME/dotfiles/Preferences.sublime-settings" "$HOME/Library/Application Support/Sublime Text 3/Packages/User"
