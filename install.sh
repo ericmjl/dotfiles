3f79bd0ea3eda8d94619eeb7e8852958eff033d1
Eric J. Ma <ericmajinglong@gmail.com>
2016-05-04 13:50:41 -0400

    added documentation to install script

--- install.sh
+++ install.sh
@@ -1,8 +1,15 @@
 # Symlink bash_profile and bashrc to point to dotfiles
 ln -svf "$HOME/dotfiles/.bash_profile" ~
 ln -svf "$HOME/dotfiles/.bashrc" ~
+
+# Symlink condarc
 ln -svf "$HOME/dotfiles/.condarc" ~
+
+# Symlink the sublime 3 preferences.
 ln -svf "$HOME/dotfiles/Preferences.sublime-settings" "$HOME/Library/Application Support/Sublime Text 3/Packages/User"
+
+# Symlink gitconfig
 ln -svf "$HOME/dotfiles/.gitconfig" ~
 
+# Symlink script for updating all git repositories
 ln -svf "$HOME/dotfiles/scripts/update_git_repos.sh" "$github"
