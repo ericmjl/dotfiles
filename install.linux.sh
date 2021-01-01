6ce385080f7081dcc05b8329afd7dfa658b1c23b
Eric Ma <ericmajinglong@gmail.com>
2020-12-31 19:51:39 -0500

    Trying out a bunch of individual builds

--- install.linux.sh
+++ install.linux.sh
@@ -23,3 +23,7 @@ chmod +x ./scripts/install_nanorc.sh
 # Step 6: Install oh-my-zsh
 chmod +x : ./scripts/install_ohmyzsh.sh
 ./scripts/install_ohmyzsh.sh
+
+# Step 7: Install Heroku
+chmod +x: ./scripts/install_heroku-linux.sh
+./scripts/install_heroku-linux.sh
--- scripts/install_homebrew_packages.sh
+++ scripts/install_homebrew_packages.sh
@@ -1,28 +1,21 @@
 
 BREW_PACKAGES=(
-  "bash-completion"     # bash completion for git and other things https://github.com/scop/bash-completion
-  "bat"			            # A replacement for cat:  https://github.com/sharkdp/bat
-  "diff-so-fancy"       # A replacement for diff: https://github.com/so-fancy/diff-so-fancy
-  "exa"                 # an enhanced, modern ls
-  "exiftool"            # just in case
-  "fd"			            # A replacement for find: https://github.com/sharkdp/fd
-  "freetype"            # for worship-manager
-  "gcc"                 # gnu compiler collection https://gcc.gnu.org/
-  "git"                 # version control https://git-scm.com/
-  "gnu-sed"             # get "standard" sed that is used on Linux.
-  "heroku-cli"          # for heroku apps
-  "heroku/brew/heroku"  # for heroku
-  "imagemagick"         # for worship-manager
-  "libmagic"            # for worship-manager
-  "libreoffice"         # for worship-manager
-  "micro"               # micro text editor
-  "mobile-shell"        # mobile shell
-  "nano"                # get an enhanced nano
-  "ripgrep"             # A replacement for grep: https://github.com/BurntSushi/ripgrep
-  "tmux"                # window management https://tmux.github.io/
-  "tree"                # get the standard "tree" command on Linux.
-  "wget"                # curl alternative https://www.gnu.org/software/wget/
-  "zsh"                 # z-shell
+  "bash-completion" # bash completion for git and other things https://github.com/scop/bash-completion
+  "bat"			        # A replacement for cat:  https://github.com/sharkdp/bat
+  "diff-so-fancy"   # A replacement for diff: https://github.com/so-fancy/diff-so-fancy
+  "exa"             # an enhanced, modern ls
+  "fd"			        # A replacement for find: https://github.com/sharkdp/fd
+  "gcc"             # gnu compiler collection https://gcc.gnu.org/
+  "git"             # version control https://git-scm.com/
+  "gnu-sed"         # get "standard" sed that is used on Linux.
+  "micro"           # micro text editor
+  "mobile-shell"    # mobile shell
+  "nano"            # get an enhanced nano
+  "ripgrep"         # A replacement for grep: https://github.com/BurntSushi/ripgrep
+  "tmux"            # window management https://tmux.github.io/
+  "tree"            # get the standard "tree" command on Linux.
+  "wget"            # curl alternative https://www.gnu.org/software/wget/
+  "zsh"             # z-shell
 )
 
 # Install homebrew packages that might not already be installed.
